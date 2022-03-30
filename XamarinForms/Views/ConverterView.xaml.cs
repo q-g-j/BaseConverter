5d66860646f7ef4f5dc59fb7d4b5bf6881a9346b
QGJ <qgj.devel@gmail.com>
2022-03-30 20:08:04 +0200

    added iOS version, renamed all projects

---
+++ XamarinForms/Views/ConverterView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BaseConverter.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConverterView : ContentPage
+    {
+        public ConverterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
